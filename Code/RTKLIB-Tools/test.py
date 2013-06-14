e47a16fc1fdfbb8bd3fe3788c62be16aeb458e32
Ruffin <roxfoxpox@gmail.com>
2013-06-14 17:24:43 -0400

    Added bash code

---
+++ Code/RTKLIB-Tools/test.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 14, 2013
+
+@author: ruffin
+'''
+
+
+if __name__ == '__main__':
+    pass
+
+suffix = 'wow!!!';
+print(suffix)
+print(suffix.endswith(suffix))
+
+weight = float(input("How many pounds does your suitcase weigh? "))
+if weight > 50:
+    print("There is a $25 charge for luggage that heavy.")
+print("Thank you for your business.")
--- Code/RTKLIB-Tools/ublox_script.py
+++ Code/RTKLIB-Tools/ublox_script.py
@@ -3,41 +3,68 @@ Created on Jun 10, 2013
 
 @author: whitemrj
 '''
+from symbol import if_stmt
 
 if __name__ == '__main__':
     pass
 
 from ftplib import FTP
-ftp = FTP('ftp.ngs.noaa.gov')
-ftp.login()
-ftp.getwelcome()
-ftp.retrlines('LIST')
+import subprocess
+import os, sys
+import sh
+#------------------------------------------------------------------------------ 
+# File Directory
+#------------------------------------------------------------------------------ 
+indir = "/home/ruffin/Documents/Data/in/"
+outdir = "/home/ruffin/Documents/Data/out/"
+obsFile = '/home/ruffin/Documents/Data/in/COM10_130604_230821.obs'
 
-file = 'paap1580.13S'
-hostPath = '/cors/rinex/2013/158/paap'
-clientPath = '/home/ruffin/Documents/Data/'
+#------------------------------------------------------------------------------ 
+# Check output directory can be dumped to
+#------------------------------------------------------------------------------ 
+if not outdir.endswith('/'):
+    outdir += '/'
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
 
-f = open(clientPath + file,'wb')  
+#------------------------------------------------------------------------------ 
+# Get log gps time from rinex file
+#------------------------------------------------------------------------------ 
+lol = subprocess.Popen('./ls', cwd='/bin')
+lol.wait()
+print(lol.stdout)
 
-ftp.cwd(hostPath)
 
-ftp.retrbinary('RETR ' + file, f.write)
-
+# print(subprocess.check_output(['grep', '"TIME OF FIRST OBS"', obsFile])
 
 
-# ftp.retrbinary("RETR paap1580.13S", open('paap1580.13S', 'wb').write)
 
-f.close()
-ftp.quit()
 
-# ftp://www.ngs.noaa.gov/cors/rinex/2013/158/paap/paap1580.13S
-
-print('Starting ublox script\n')
-print('Checking ublox logs\n')
-print('Converting ublox logs\n')
-print('Parsing time from data\n')
-print('Fetching NOAA CORS corrections\n')
-print('Fetching station broadcasts\n')
-print('Fetching updated ephemerides\n')
-print('Uncompressing fetched data\n')
-print('Running RTK solution\n')
+# ftp = FTP('ftp.ngs.noaa.gov')
+# ftp.login()
+# ftp.getwelcome()
+# ftp.retrlines('LIST')
+# 
+# file = 'paap1580.13S'
+# hostPath = '/cors/rinex/2013/158/paap'
+# clientPath = '/home/ruffin/Documents/Data/'
+# 
+# f = open(clientPath + file,'wb')  
+# 
+# ftp.cwd(hostPath)
+# 
+# ftp.retrbinary('RETR ' + file, f.write)
+# f.close()
+# ftp.quit()
+# 
+# 
+# 
+# print('Starting ublox script\n')
+# print('Checking ublox logs\n')
+# print('Converting ublox logs\n')
+# print('Parsing time from data\n')
+# print('Fetching NOAA CORS corrections\n')
+# print('Fetching station broadcasts\n')
+# print('Fetching updated ephemerides\n')
+# print('Uncompressing fetched data\n')
+# print('Running RTK solution\n')
