61a2f4f93ac92dfdf8b57d5707859b23e37f0f3f
Ruffin <roxfoxpox@gmail.com>
2013-07-01 20:17:19 -0400

    Added scatter plots Include more info on relation of distance and standard deviation

--- Code/RTKLIB-Tools/gps_pos_plot.py
+++ Code/RTKLIB-Tools/gps_pos_plot.py
@@ -3,6 +3,10 @@ Created on Jun 26, 2013
 
 @author: ruffin
 '''
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import normalize
+from numpy.ma.core import sqrt
+from matplotlib.colorbar import Colorbar
 if __name__ == '__main__':
     pass
 # from pylab import *
@@ -20,15 +24,15 @@ from fileutils import *
 #------------------------------------------------------------------------------ 
 indir = '/home/ruffin/Documents/Data/in/'
 outdir = '/home/ruffin/Documents/Data/out/'
-k = point.Point(40.442635758, -79.943065017)
-# k = Point(40.443874, -79.945517)
+# k = point.Point(40.442635758, -79.943065017)
+k = point.Point(40.443874, -79.945517)
 
 
 #------------------------------------------------------------------------------ 
 # Check output directory can be dumped to
 #------------------------------------------------------------------------------ 
 indir = checkDir(indir,'r')
-outdir = checkDir(outdir,'w')
+outdir = checkDir(outdir,'r')
 
 #------------------------------------------------------------------------------ 
 # Get log gps file name
@@ -68,10 +72,10 @@ for i in data.T:
 #------------------------------------------------------------------------------ 
 # Create a new figure of size 10x6 points, using 80 dots per inch
 lp.figure(figsize=(10,6), dpi=80)
-
+  
 # Create a new subplot from a grid of 1x1
 lp.subplot(1,1,1)
-
+  
 # Make plots
 lp.plot(tdate,sdn, label='sdn')
 lp.plot(tdate,sde, label='sde')
@@ -80,12 +84,134 @@ lp.plot(tdate,sdne, label='sdne')
 lp.plot(tdate,sdeu, label='sdeu')
 lp.plot(tdate,sdun, label='sdun')
 lp.plot(tdate,dist, label='dist')
-
+  
 # Make legend
 lp.legend(loc='upper left')
-
+  
 # Save figure using 72 dots per inch
 # lp.savefig("exercice_2.png",dpi=72)
+   
+# # Show result on screen
+# lp.show()
+
+distmax = dist.max()
+distmin = dist.min()
+print(distmax)
+print(distmin)
+
  
+fig2 = lp.figure(figsize=(10,6), dpi=80)
+fig2.suptitle('Correlation of Standard Distribution vs Distance', fontsize=14, fontweight='bold')
+ 
+sd = ['sdn','sde','sdu','sdne','sdeu','sdun']
+for i in range(6):
+    sdx = data[i+6]
+    color = -sqrt(sdx**2+dist**2)
+    ax = fig2.add_subplot(2,3,i+1)
+    ax.scatter(sdx,dist, c=color, alpha=.2)
+    ax.set_title(sd[i]+' vs dist')
+    ax.set_ylabel('dist (m)')
+    ax.set_xlabel(sd[i]+' (m)')
+    ax.grid(True)
+ 
+# # Show result on screen
+# lp.show()
+
+color = colors()
+latmin = lat.min()
+latmax = lat.max()
+lonmin = lon.min()
+lonmax = lon.max()
+
+latdif = latmax - latmin
+londif = lonmax - lonmin
+
+latminlim = lat.min() - latdif*0.1
+latmaxlim = lat.max() + latdif*0.1
+lonminlim = lon.min() - londif*0.1
+lonmaxlim = lon.max() + londif*0.1
+
+
+fig3 = lp.figure(figsize=(10,6), dpi=80)
+fig3.suptitle('Position and Standard Distribution', fontsize=14, fontweight='bold')
+
+sd = ['sdn','sde','sdu','sdne','sdeu','sdun']
+
+for i in range(6):
+    sdx = data[i+6]
+    ax = fig3.add_subplot(2, 3, i+1)
+    p = ax.scatter(lat,lon, c=-sdx, alpha=.2)
+    fig3.colorbar(p)
+    xlim(latminlim, latmaxlim)
+    ylim(lonminlim, lonmaxlim)    
+    ax.set_title('Pos w/ ' + sd[i])
+    ax.set_xlabel('lat (deg)')
+    ax.set_ylabel('lon (deg)')
+    ax.get_xaxis().set_ticks([])
+    ax.get_yaxis().set_ticks([])
+    ax.grid(True)
+
+# 3D stuff
+# for i in range(1):
+#     sdx = data[i+6]
+#     ax = fig3.add_subplot(1, 1, i+1, projection='3d')
+#     
+#     ax.scatter(lat,lon, sdx, c=dist, edgecolors = 'none')
+#     xlim(latminlim, latmaxlim)
+#     ylim(lonminlim, lonmaxlim)
+#     ax.set_title('Pos w/ ' + sd[i])
+#     ax.set_xlabel('lat (deg)')
+#     ax.set_ylabel('lon (deg)')
+#     ax.set_ylabel(sd[i] +' (m)')
+
+
+# lp.subplot(2,3,1)
+# scatter(lat,lon, c=sdn, edgecolors = 'none')
+# xlim(latminlim, latmaxlim)
+# ylim(lonminlim, lonmaxlim)
+# lp.subplot(2,3,2)
+# scatter(lat,lon, c=sde, edgecolors = 'none')
+# xlim(latminlim, latmaxlim)
+# ylim(lonminlim, lonmaxlim)
+# lp.subplot(2,3,3)
+# scatter(lat,lon, c=sdu, edgecolors = 'none')
+# xlim(latminlim, latmaxlim)
+# ylim(lonminlim, lonmaxlim)
+# lp.subplot(2,3,4)
+# scatter(lat,lon, c=sdne, edgecolors = 'none')
+# xlim(latminlim, latmaxlim)
+# ylim(lonminlim, lonmaxlim)
+# lp.subplot(2,3,5)
+# scatter(lat,lon, c=sdeu, edgecolors = 'none')
+# xlim(latminlim, latmaxlim)
+# ylim(lonminlim, lonmaxlim)
+# lp.subplot(2,3,6)
+# scatter(lat,lon, c=sdun, edgecolors = 'none')
+# xlim(latminlim, latmaxlim)
+# ylim(lonminlim, lonmaxlim)
+
 # Show result on screen
+
+fig4 = lp.figure(figsize=(10,6), dpi=80)
+fig4.suptitle('Position and Standard Distribution', fontsize=14, fontweight='bold')
+
+sd = ['sdn','sde','sdu','sdne','sdeu','sdun']
+
+ax = fig4.add_subplot(1, 1, 1)
+cm = lp.cm.get_cmap('RdYlBu')
+p = ax.scatter(lat,lon, c=-dist, alpha=.2)
+fig4.colorbar(p)
+xlim(latminlim, latmaxlim)
+ylim(lonminlim, lonmaxlim)    
+ax.set_title('Pos w/ ' + sd[i])
+ax.set_xlabel('lat (deg)')
+ax.set_ylabel('lon (deg)')
+ax.grid(True)
+
+
+# lp.colorbar(ax, cmap=dist, orientation='horizontal')
+# cbar = ax.add_colorbar(dist, orientation='horizontal')
+# cbar.ax.set_xticklabels(['Low', 'Medium', 'High'])# horizontal colorbar
+
+
 lp.show()
